e7b8406b5648da50a63d6a1e2a1200fc4d23629b
azrifarhan <141860313+azrifarhan@users.noreply.github.com>
2024-06-16 18:11:32 +0800

    Update F1DB24 Dashboard Base.py

--- F1DB24 Dashboard Base.py
+++ F1DB24 Dashboard Base.py
@@ -1,23 +1,35 @@
 import pandas as pd
 import plotly.express as px  # (version 4.7.0 or higher)
 import plotly.graph_objects as go
-from dash import Dash, dcc, html, Input, Output,callback  # pip install dash (version 2.0.0 or higher)
+from dash import Dash, dcc, html, Input, Output,callback
 import dash_bootstrap_components as dbc
+from fetch_query import query_fetch
 
-df = pd.read_csv("C:\\Users\\Azri\\Python Wasteland\\BAHRAIN GP 2023\\240416FINALSLTBAHRAIN23.csv")
-minisecdf = pd.read_csv(r'F:\Python Wasteland\BAHRAIN GP 2023\minisecBahrain23.csv')
-speedf = pd.read_csv(r'F:\Python Wasteland\BAHRAIN GP 2023\minisecspeedBahrain23.csv')
+data = query_fetch()
+df = data['Laptimes']
+minisecdf = data['MSEC']
+speedf = data['MSECAVG']
 app = Dash(external_stylesheets=[dbc.themes.DARKLY])
+
 chartstyle = {'display': 'inline-block',
               'background-color':'rgba(38,50,56,1)',
               'border' :'1px',
               'border-radius':'10px',
               'padding':'0px',
               'margin':'10px'}
-app.title = "F1 Bahrain 2023"
+
+app.title = "🏎️ F1DB24 🏎️"
 app.layout = html.Div(
         className="app-div",
-        children=[html.H1(app.title),
+        children=[html.Div(className = 'Title Header', style={
+                       'display': 'flex',
+                       'flexDirection': 'column',
+                      'justify-content': 'center',
+                      'paddingTop': '20px',
+                      'align-items': 'center',
+                      'width': '100%'},
+                      children=[html.H1(app.title),
+                                html.P('Missed the race? Get up to speed with F1DB24!')]),
                   html.Hr(),
                   html.Div(className = 'Controls',style={
                        'display': 
@@ -30,18 +42,18 @@ app.layout = html.Div(
                           id='dropdown',
                           style={'display': 'inline-block'},
                           options=[
-                              {'label':i,'value':i} for i in df['Driver'].unique()
+                              {'label':i,'value':i} for i in df['Nick'].unique()
                               ],
-                              value=df.Driver.unique(),
+                              value=df.Nick.unique(),
                               multi=True , 
                               placeholder='Choose Drivers')]),
                     html.Div(id = 'control2',
                         children = [
                             dcc.RangeSlider(
                               id = 'LapSlider',
-                              min = df['Laps(?)'].min(),
-                              max = df['Laps(?)'].max(),
-                              value = [df['Laps(?)'].min(),df['Laps(?)'].max()],
+                              min = df['Laps'].min(),
+                              max = df['Laps'].max(),
+                              value = [df['Laps'].min(),df['Laps'].max()],
                               step = 1)]),
                     html.Div(className = 'ChartA',style={
                       'display': 
@@ -61,7 +73,7 @@ app.layout = html.Div(
                         children=[
                          dcc.Graph(id='tyrestrat',style=chartstyle),
                          dcc.Graph(id='minisec',style=chartstyle)])])
-app.title = "F1TEST"
+
 @callback(
     Output('laptime', 'figure'),
     Output('boxplot','figure'),
@@ -81,22 +93,22 @@ def update_figure(dropdown,LapSlider):
                'plot_bgcolor':'rgba(0,0,0,0)',
                'title_font':font_dict,
                'font':font_dict}
-    newdf = df[(df.Driver.isin(dropdown)) & (df['Laps(?)'] >= LapSlider[0]) & (df['Laps(?)'] <= LapSlider[1])  ]
-    boxdf = newdf[(df['Laps(?)'] >= LapSlider[0]) & (df['Laps(?)'] <= LapSlider[1]) & (newdf['PitIn'].isnull() & newdf['PitOut'].isnull()) & (newdf['TrackStat'] == 1)]
-    newspf = speedf[speedf['Driver'].isin(dropdown)]
+    newdf = df[(df.Nick.isin(dropdown)) & (df['Laps'] >= LapSlider[0]) & (df['Laps'] <= LapSlider[1])  ]
+    boxdf = newdf[(newdf['PitIn'].isnull() & newdf['PitOut'].isnull()) & (newdf['TrackStat'] == '1')]
+    newspf = speedf[speedf['Nick'].isin(dropdown)]
     newspf['Rank'] = newspf.groupby(['Minisector'])['Speed'].rank(ascending=False)
     best_speed = newspf[newspf['Rank']<2]
     plotsec = pd.merge(minisecdf,best_speed,on='Minisector')
     lapfig=px.line(newdf, 
-                   x='Laps(?)',
+                   x='Laps',
                    y='Smoothed',
-                   color='Driver',
+                   color='Nick',
                    title = 'Smoothed Lap Times')
-    boxfig = px.box(boxdf, x="Driver", y="LapTime",title='Lap Time Distribution',color='red')
-    secfig  = px.scatter(plotsec, y="y", x="x", color="Driver",title='Minisector Average Speed')
+    boxfig = px.box(boxdf, x="Nick", y="LapTime",title='Lap Time Distribution')
+    secfig  = px.scatter(plotsec, y="y", x="x", color="Nick",title='Minisector Average Speed')
     secfig.update_xaxes(visible=False)
     secfig.update_yaxes(visible=False)
-    tyrestrat = px.scatter(newdf, y='Driver',x='Laps(?)',color = 'Tyres',symbol='Stint',color_discrete_map=tyre_col,title='Tyre Strategy')
+    tyrestrat = px.scatter(newdf, y='Nick',x='Laps',color = 'Tyres',symbol='Stint',color_discrete_map=tyre_col,title='Tyre Strategy')
     tyrestrat.update_layout(title_font=font_dict,showlegend=False)
     tyrestrat.update_layout(bg_dict)
     boxfig.update_layout(bg_dict)
